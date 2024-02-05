46a738ba1debe30ad5038e37f757617ee1b43483
Hannah Rogers <rogershannah99@gmail.com>
2024-02-05 16:11:42 -0600

    Other attributes
    color attributes!

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -1,10 +1,8 @@
-
 #version 330 core
-out vec4 FragColor;
+out vec4 FragColor;  
+in vec3 ourColor;
   
-uniform vec4 vertexColor; // the input variable from the vertex shader (same name and same type)  
-
 void main()
 {
-    FragColor = vertexColor;
-} 
+    FragColor = vec4(ourColor, 1.0);
+}
--- src/GLFWGraphicsProgram.cpp
+++ src/GLFWGraphicsProgram.cpp
@@ -109,12 +109,6 @@ void GLFWGraphicsProgram::Render()
 
     glUseProgram(shader);
 
-    // update the uniform color
-    float timeValue = glfwGetTime();
-    float greenValue = sin(timeValue) / 2.0f + 0.5f;
-    int vertexColorLocation = glGetUniformLocation(shader, "vertexColor");
-    glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-
     //render triangle
     glBindVertexArray(VAO);
     glDrawArrays(GL_TRIANGLES, 0, 3);
@@ -179,6 +173,9 @@ void GLFWGraphicsProgram::GenerateBuffers()
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
 }
 
