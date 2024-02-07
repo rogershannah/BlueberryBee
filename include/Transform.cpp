20194e70b3c0c2236b0c2c251bbb6ba8de533f2d
Hannah Rogers <rogershannah99@gmail.com>
2024-02-07 13:00:44 -0600

    Stub out Transform functions

--- include/Transform.cpp
+++ include/Transform.cpp
@@ -7,3 +7,33 @@ Transform::Transform()
 Transform::~Transform()
 {
 }
+
+void Transform::LoadIdentity()
+{
+}
+
+void Transform::Translate(float x, float y, float z)
+{
+}
+
+void Transform::Rotate(float radians, float x, float y, float z)
+{
+}
+
+void Transform::Scale(float x, float y, float z)
+{
+}
+
+GLfloat* Transform::GetTransformMatrix()
+{
+	return nullptr;
+}
+
+void Transform::ApplyTransform(Transform t)
+{
+}
+
+glm::mat4 Transform::GetInternalMatrix()
+{
+	return glm::mat4();
+}
--- include/Transform.h
+++ include/Transform.h
@@ -3,8 +3,11 @@
 #define Transform_HPP
 
 #include <glad/glad.h>
-#include <string>
-#include "stb_image.h";
+#include "glm/vec3.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class Transform {
 public:
@@ -12,8 +15,26 @@ public:
     Transform();
     // Destructor
     ~Transform();
-    
+
+    // Resets matrix transformations to the identity matrix
+    void LoadIdentity();
+    // Performs a translation of an object
+    void Translate(float x, float y, float z);
+    // Performs rotation about an axis
+    void Rotate(float radians, float x, float y, float z);
+    // Performs rotation about an axis
+    void Scale(float x, float y, float z);
+    // Returns the transformation matrix
+    GLfloat* GetTransformMatrix();
+
+    // Takes in a transform and sets internal matrix.
+    void ApplyTransform(Transform t);
+    // Returns the transformation matrix
+    glm::mat4 GetInternalMatrix();
+
 private:
+    // Stores the actual transformation matrix
+    glm::mat4 m_modelTransformMatrix;
     
 };
 
