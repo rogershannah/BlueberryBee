0ac1688c6b8c8fe279b0855207fee6b69fab386e
Hannah Rogers <rogershannah99@gmail.com>
2024-02-13 12:58:50 -0600

    texture shaders - re-add

---
+++ shaders/basicTextureFrag.glsl
@@ -0,0 +1,20 @@
+#version 330 core
+out
+vec4 FragColor;
+
+in
+vec3 ourColor;
+in
+vec2 TexCoord;
+
+uniform float mixValue;
+
+// texture samplers
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main()
+{
+	// linearly interpolate
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), mixValue);
+}
